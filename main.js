3e51a141771ae34c4b4fa3d667ff7943a319492c
Kraktoos <kraktoos@gmail.com>
2022-08-26 01:37:51 +0100

    made it infinitely playable

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
-startGameBtn = document.getElementById("start-game-btn");
+startGameBtn = document.createElement("button");
+startGameBtn.className = "start-game-btn";
+startGameBtn.innerHTML = "Start Game";
+
 let isAlive;
 let closedCount;
 let windowCount;
 let finishedCount;
 let score;
-let interval = 5500;
+let interval = 6500;
 
 const modules = [
   {
@@ -12,46 +15,48 @@ const modules = [
     correctScore: 20,
     createFunction: function (current) {
       let window = createWindow("Train Aim");
-      window.style.top = Math.random() * 75 + "%";
-      window.style.left = Math.random() * 79 + "%";
-      const windowContent = document.createElement("div");
-      windowContent.className = "aim-train";
+      if (window) {
+        window.style.top = Math.random() * 75 + "%";
+        window.style.left = Math.random() * 79 + "%";
+        const windowContent = document.createElement("div");
+        windowContent.className = "aim-train";
 
-      let counter = {
-        count: 0,
-        add: function () {
-          this.count++;
-          this.checkChange();
-        },
-      };
-      counter.checkChange = function () {
-        if (this.count === MAX) {
-          window.remove();
-          finishedCount++;
-          score += current.correctScore;
-          console.log(score);
+        let counter = {
+          count: 0,
+          add: function () {
+            this.count++;
+            this.checkChange();
+          },
+        };
+        counter.checkChange = function () {
+          if (this.count === MAX) {
+            window.remove();
+            finishedCount++;
+            score += current.correctScore;
+            console.log(score);
+          }
+        };
+        const MAX = 3;
+
+        for (let i = 0; i < MAX; i++) {
+          const target = document.createElement("img");
+          target.src = "assets/target.png";
+          target.alt = "target";
+          target.className = "shoot-me";
+          target.style.width = Math.random() * 2.2 + 2 + "vw";
+          target.style.top = 72 * Math.random() + "%";
+          target.style.left = 80 * Math.random() + "%";
+          target.addEventListener("click", (event) => {
+            target.style.display = "none";
+            counter.add();
+          });
+          windowContent.appendChild(target);
         }
-      };
-      const MAX = 3;
 
-      for (let i = 0; i < MAX; i++) {
-        const target = document.createElement("img");
-        target.src = "assets/target.png";
-        target.alt = "target";
-        target.className = "shoot-me";
-        target.style.width = Math.random() * 2.2 + 2 + "vw";
-        target.style.top = 72 * Math.random() + "%";
-        target.style.left = 80 * Math.random() + "%";
-        target.addEventListener("click", (event) => {
-          target.style.display = "none";
-          counter.add();
-        });
-        windowContent.appendChild(target);
+        window.appendChild(windowContent);
+
+        return window;
       }
-
-      window.appendChild(windowContent);
-
-      return window;
     },
   },
 
@@ -60,42 +65,44 @@ const modules = [
     correctScore: 50,
     createFunction: function (current) {
       let window = createWindow("Captcha");
-      window.style.top = Math.random() * 75 + "%";
-      window.style.left = Math.random() * 79 + "%";
-      const windowContent = document.createElement("div");
-      windowContent.className = "captcha";
+      if (window) {
+        window.style.top = Math.random() * 75 + "%";
+        window.style.left = Math.random() * 79 + "%";
+        const windowContent = document.createElement("div");
+        windowContent.className = "captcha";
 
-      const captchaCode = document.createElement("p");
-      captchaCode.className = "captcha-code";
-      const alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "abcdefghijklmnopqrstuvwxyz" + "0123456789";
-      for (let i = 0; i < 6; i++) {
-        captchaCode.innerHTML += alpha.charAt(Math.floor(Math.random() * alpha.length));
-      }
-
-      const input = document.createElement("input");
-      input.className = "captcha-input";
-
-      const button = document.createElement("button");
-      button.className = "captcha-btn";
-      button.innerHTML = "Submit";
-      button.addEventListener("click", (event) => {
-        if (input.value === captchaCode.innerHTML) {
-          window.remove();
-          finishedCount++;
-          score += current.correctScore;
-        } else {
-          window.remove();
-          closedCount++;
-          score -= 10;
+        const captchaCode = document.createElement("p");
+        captchaCode.className = "captcha-code";
+        const alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "abcdefghijklmnopqrstuvwxyz" + "0123456789";
+        for (let i = 0; i < 6; i++) {
+          captchaCode.innerHTML += alpha.charAt(Math.floor(Math.random() * alpha.length));
         }
-      });
 
-      windowContent.appendChild(captchaCode);
-      windowContent.appendChild(input);
-      windowContent.appendChild(button);
-      window.appendChild(windowContent);
+        const input = document.createElement("input");
+        input.className = "captcha-input";
 
-      return window;
+        const button = document.createElement("button");
+        button.className = "captcha-btn";
+        button.innerHTML = "Submit";
+        button.addEventListener("click", (event) => {
+          if (input.value === captchaCode.innerHTML) {
+            window.remove();
+            finishedCount++;
+            score += current.correctScore;
+          } else {
+            window.remove();
+            closedCount++;
+            score -= 10;
+          }
+        });
+
+        windowContent.appendChild(captchaCode);
+        windowContent.appendChild(input);
+        windowContent.appendChild(button);
+        window.appendChild(windowContent);
+
+        return window;
+      }
     },
   },
 ];
@@ -129,24 +136,44 @@ function createWindow(title) {
   }
 }
 
-startGameBtn.addEventListener("click", (event) => {
+function resetGame() {
   isAlive = true;
   closedCount = 0;
   finishedCount = 0;
   windowCount = 0;
   score = 0;
-  startGameBtn.style.display = "none";
+  document.querySelector(".wallpaper").innerHTML = "";
+  const newStartGameBtn = startGameBtn.cloneNode(true);
 
-  timer = function () {
-    interval *= 0.97 + Math.random() * 0.025;
-    interval += Math.random() * 0.04;
-    randomModule = modules[Math.floor(Math.random() * modules.length)];
-    document.querySelector(".wallpaper").appendChild(new randomModule.createFunction(randomModule));
-    console.log("Current Windows: " + (windowCount - closedCount - finishedCount));
-    console.log(isAlive);
-    if (isAlive) {
-      setTimeout(timer, interval);
+  newStartGameBtn.addEventListener("click", (event) => {
+    if (!isAlive) {
+      resetGame();
     }
-  };
-  timer();
-});
+
+    newStartGameBtn.remove();
+
+    timer = function () {
+      interval *= 0.97 + Math.random() * 0.025;
+      interval += Math.random() * 0.04;
+      randomModule = modules[Math.floor(Math.random() * modules.length)];
+      if (isAlive) {
+        const window = randomModule.createFunction(randomModule);
+        if (window) {
+          document.querySelector(".wallpaper").appendChild(window);
+        }
+      }
+      console.log("Current Windows: " + (windowCount - closedCount - finishedCount));
+      console.log(isAlive);
+      if (isAlive) {
+        setTimeout(timer, interval);
+      } else {
+        resetGame();
+      }
+    };
+    timer();
+  });
+
+  document.querySelector(".wallpaper").appendChild(newStartGameBtn);
+}
+
+resetGame();
