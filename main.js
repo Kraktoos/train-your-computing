7dd6bccc733d535ce6463ee5ff33418cef4919bf
Kraktoos <kraktoos@gmail.com>
2022-08-26 12:20:22 +0100

    copied and pasted code that made windows draggable

--- main.js
+++ main.js
@@ -157,7 +157,7 @@ function createWindow(title) {
     let window = document.createElement("div");
     window.className = "window-borders";
     const flexboxContainer = document.createElement("div");
-    flexboxContainer.className = "flexbox-container";
+    flexboxContainer.className = "window-bordersheader";
     const windowTitle = document.createElement("p");
     windowTitle.className = "module-title";
     windowTitle.innerHTML = title;
@@ -172,6 +172,45 @@ function createWindow(title) {
     flexboxContainer.appendChild(windowTitle);
     flexboxContainer.appendChild(button);
     window.appendChild(flexboxContainer);
+    dragElement(window);
+    function dragElement(elmnt) {
+      var pos1 = 0,
+        pos2 = 0,
+        pos3 = 0,
+        pos4 = 0;
+      // otherwise, move the DIV from anywhere inside the DIV:
+      elmnt.getElementsByClassName("window-bordersheader")[0].onmousedown = dragMouseDown;
+
+      function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+      }
+
+      function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        elmnt.style.top = elmnt.offsetTop - pos2 + "px";
+        elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
+      }
+
+      function closeDragElement() {
+        // stop moving when mouse button is released:
+        document.onmouseup = null;
+        document.onmousemove = null;
+      }
+    }
     return window;
   } else {
     isAlive = false;
