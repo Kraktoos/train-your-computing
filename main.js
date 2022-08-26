c998a032af87143e7c00eb8e97d722f982ad99c8
Kraktoos <kraktoos@gmail.com>
2022-08-26 01:15:59 +0100

    added Captcha

--- main.js
+++ main.js
@@ -3,14 +3,15 @@ let isAlive;
 let closedCount;
 let windowCount;
 let finishedCount;
-let interval = 2500;
+let score;
+let interval = 5500;
 
 const modules = [
   {
     className: "aim-train",
-    correctScore: 40,
-    createFunction: function () {
-      let window = createWindow();
+    correctScore: 20,
+    createFunction: function (current) {
+      let window = createWindow("Train Aim");
       window.style.top = Math.random() * 75 + "%";
       window.style.left = Math.random() * 79 + "%";
       const windowContent = document.createElement("div");
@@ -27,6 +28,8 @@ const modules = [
         if (this.count === MAX) {
           window.remove();
           finishedCount++;
+          score += current.correctScore;
+          console.log(score);
         }
       };
       const MAX = 3;
@@ -51,22 +54,73 @@ const modules = [
       return window;
     },
   },
+
+  {
+    className: "captcha",
+    correctScore: 50,
+    createFunction: function (current) {
+      let window = createWindow("Captcha");
+      window.style.top = Math.random() * 75 + "%";
+      window.style.left = Math.random() * 79 + "%";
+      const windowContent = document.createElement("div");
+      windowContent.className = "captcha";
+
+      const captchaCode = document.createElement("p");
+      captchaCode.className = "captcha-code";
+      const alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "abcdefghijklmnopqrstuvwxyz" + "0123456789";
+      for (let i = 0; i < 6; i++) {
+        captchaCode.innerHTML += alpha.charAt(Math.floor(Math.random() * alpha.length));
+      }
+
+      const input = document.createElement("input");
+      input.className = "captcha-input";
+
+      const button = document.createElement("button");
+      button.className = "captcha-btn";
+      button.innerHTML = "Submit";
+      button.addEventListener("click", (event) => {
+        if (input.value === captchaCode.innerHTML) {
+          window.remove();
+          finishedCount++;
+          score += current.correctScore;
+        } else {
+          window.remove();
+          closedCount++;
+          score -= 10;
+        }
+      });
+
+      windowContent.appendChild(captchaCode);
+      windowContent.appendChild(input);
+      windowContent.appendChild(button);
+      window.appendChild(windowContent);
+
+      return window;
+    },
+  },
 ];
 
-function createWindow() {
+function createWindow(title) {
   if (windowCount - closedCount - finishedCount < 10) {
     windowCount++;
     let window = document.createElement("div");
     window.className = "window-borders";
+    const flexboxContainer = document.createElement("div");
+    flexboxContainer.className = "flexbox-container";
+    const windowTitle = document.createElement("p");
+    windowTitle.className = "module-title";
+    windowTitle.innerHTML = title;
     const button = document.createElement("button");
     button.className = "close-btn";
     button.innerHTML = "&times;";
     button.addEventListener("click", (event) => {
       window.remove();
       closedCount++;
-      console.log(closedCount);
+      score -= 10;
     });
-    window.appendChild(button);
+    flexboxContainer.appendChild(windowTitle);
+    flexboxContainer.appendChild(button);
+    window.appendChild(flexboxContainer);
     return window;
   } else {
     isAlive = false;
@@ -80,12 +134,14 @@ startGameBtn.addEventListener("click", (event) => {
   closedCount = 0;
   finishedCount = 0;
   windowCount = 0;
+  score = 0;
   startGameBtn.style.display = "none";
 
   timer = function () {
-    interval *= 0.967 + Math.random() * 0.025;
+    interval *= 0.97 + Math.random() * 0.025;
     interval += Math.random() * 0.04;
-    document.querySelector(".wallpaper").appendChild(new modules[0].createFunction());
+    randomModule = modules[Math.floor(Math.random() * modules.length)];
+    document.querySelector(".wallpaper").appendChild(new randomModule.createFunction(randomModule));
     console.log("Current Windows: " + (windowCount - closedCount - finishedCount));
     console.log(isAlive);
     if (isAlive) {
